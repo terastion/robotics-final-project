7d7782963b6a76d04d8dc0bfbfe73975c08bdf5d
Terastion <108305109+terastion@users.noreply.github.com>
2024-05-08 18:43:44 -0500

    add range updater support for testing color ranges

--- scripts/main_node.py
+++ scripts/main_node.py
@@ -5,12 +5,23 @@ import numpy as np
 import math
 from sensor_msgs.msg import Image, LaserScan
 from geometry_msgs.msg import Twist, Vector3
+from robocourier.msg import RangeUpdate
 #from std_msgs.msg import Empty
 
 class RoboCourrier(object):
     def __init__(self):
         # initialize this node
-        rospy.init_node("robocourrier")
+        rospy.init_node("robocourier")
+
+        # provide list of color ranges to be use/be updated
+        # TODO: actually determine these values
+        self.color_ranges = {
+            "orange": [np.array([0,0,0]), np.array([0,0,0])],
+            "blue": [np.array([0,0,0]), np.array([0,0,0])]
+        }
+
+        # establish range_update subscriber
+        rospy.Subscriber("/debug/range_update", RangeUpdate, self.handle_range)
 
         # establish /camera/rgb/image_raw subscriber
         rospy.Subscriber("/camera/rgb/image_raw", Image, self.handle_image)
@@ -23,12 +34,17 @@ class RoboCourrier(object):
         self.vel_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
 
         # establish interface for group of joints making up robot arm
-        self.robot_arm = moveit_commander.MoveGroupCommander("arm")
+        #self.robot_arm = moveit_commander.MoveGroupCommander("arm")
 
         # establish interface for group of joints making up robot gripper
-        self.robot_gripper = moveit_commander.MoveGroupCommander("gripper")
+        #self.robot_gripper = moveit_commander.MoveGroupCommander("gripper")
 
         rospy.spin()
+
+    # handler for range updater
+    def handle_range(self, data):
+        self.color_ranges[data.color][int(data.is_upper)] = np.array(data.range)
+        print("updated range to {}".format(data.range))
 
     # handler for scanner
     def handle_scan(self, data):
