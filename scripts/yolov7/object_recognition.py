b94d40127a9164358f29f4efd346f1a678d9cfd0
Vanessa Lois <vlois@csil5-16.cs.uchicago.edu>
2024-05-16 06:01:05 -0500

    .

--- scripts/yolov7/object_recognition.py
+++ scripts/yolov7/object_recognition.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 import rospy
-import cv2
+import cv2, cv_bridge
 import torch
 #import numpy as np
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from utils.general import non_max_suppression, scale_coords
 from utils.plots import plot_one_box
+from models.yolo import Model
+from torch.profiler import profile, record_function, ProfilerActivity
+
 import os
 from pathlib import Path
 
@@ -17,42 +20,54 @@ class SodaDetect:
         parent_directory = current_directory.parent
         print(parent_directory)
         self.device = 'cpu'
-        self.model = torch.hub.load(str(parent_directory), 'custom', 'best.pt', source= 'local' )
-        self.model.to(self.device)
+        #self.model = torch.hub.load(str(parent_directory), 'custom', 'best.pt', source= 'local' )
+        #self.model.to(self.device)
+        yaml_file = parent_directory / 'cfg/training/yolov7.yaml'
+        best_file = parent_directory / 'best.pt'
+        self.model = Model(str(yaml_file))
+        self.model.load_state_dict(torch.load(str(best_file)), strict=False)
         self.model.eval()
 
         self.labels = ["7up", "Coke", "Pepsi", "Sprite", "Big", "Est", "Fanta"]
-
-        self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber('/camera/image_raw', Image, self.image_callback)
+        self.bridge = cv_bridge.CvBridge()
+        self.image_sub = rospy.Subscriber('/camera/rgb/image_raw', Image, self.image_callback)
         self.image_pub = rospy.Publisher('/detection_image', Image, queue_size=10)
+        inputs = torch.randn(5, 3, 224, 224)
+        with profile(
+        activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
+        with_stack=True,
+    ) as prof:
+            self.model(inputs)
+
+        # Print aggregated stats
+        print(prof.key_averages(group_by_stack_n=5).table(sort_by="self_cuda_time_total", row_limit=2))
+        
 
     def image_callback(self, msg):
-        try:
-            cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-        except CvBridgeError as e:
-            rospy.logerr("CvBridge Error: {0}".format(e))
-            return
+        image = self.bridge.imgmsg_to_cv2(msg, desired_encoding ="bgr8")
+        print("showing cam")
+        cv2.imshow("window", image)
+        cv2.waitKey(3)
         
-        input_image = self.prepare_image(cv_image)
+        input_image = self.prepare_image(image)
         with torch.no_grad():
-            results = self.model(input_image)
+            results = self.model(input_image)[0]
+            print(len(results))
             results = non_max_suppression(results, 0.4, 0.5)
 
-        processed_results = self.process_results(results, cv_image)
+        processed_results = self.process_results(results, image)
         rospy.loginfo("Detected Soda: %s", processed_results)
-
         
-        try:
-            detection_msg = self.bridge.cv2_to_imgmsg(cv_image, "bgr8")
-            self.image_pub.publish(detection_msg)
-        except CvBridgeError as e:
-            rospy.logerr("Failed to change image to ROS message: {0}".format(e))
+        detection_msg = self.bridge.cv2_to_imgmsg(image, "bgr8")
+        self.image_pub.publish(detection_msg)
+        
 
-    def prepare_image(self, cv_image, img_w = 240, img_h=320):
+    def prepare_image(self, cv_image, img_w = 320, img_h=320):
         cv_image = cv2.resize(cv_image, (img_w, img_h))
         img = cv_image[:, :, ::-1].transpose(2, 0, 1)
-        img = torch.from_numpy(img).float() / 255.0
+        img = img.copy()
+        img = torch.from_numpy(img)
+        img = img.float() / 255.0
         img = img.unsqueeze(0)
         img = img.to(self.device)
         return img
